4bb5faf77e582041bd7086ad11f6ffb21023ed29
NikitaEvs <nikitaevsykov2@gmail.com>
2024-08-22 21:03:12 +0200

    [app] Add main application
    Add main application and parser provided by the distributed algorithms
    course.

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <chrono>
+#include <iostream>
+#include <signal.h>
+#include <thread>
+
+#include "applications/fifo.hpp"
+#include "applications/lattice_agreement.hpp"
+#include "applications/perfect_links.hpp"
+#include "parser.hpp"
+
+std::function<void(int)> signal_handler;
+static void stop(int value) {
+  // reset signal handlers to default
+  signal(SIGTERM, SIG_DFL);
+  signal(SIGINT, SIG_DFL);
+
+  signal_handler(value);
+
+  // immediately stop network packet processing
+  std::cout << "Immediately stopping network packet processing.\n";
+
+  // write/flush output file if necessary
+  std::cout << "Writing output.\n";
+
+  // exit directly from signal handler
+  exit(0);
+}
+
+int main(int argc, char **argv) {
+  signal(SIGTERM, stop);
+  signal(SIGINT, stop);
+
+  // `true` means that a config file is required.
+  // Call with `false` if no config file is necessary.
+  bool requireConfig = true;
+
+  Parser parser(argc, argv);
+  parser.parse();
+
+  std::cout << std::endl;
+
+  std::cout << "My PID: " << getpid() << "\n";
+  std::cout << "From a new terminal type `kill -SIGINT " << getpid()
+            << "` or `kill -SIGTERM " << getpid()
+            << "` to stop processing packets\n\n";
+
+  std::cout << "My ID: " << parser.id() << "\n\n";
+
+  std::cout << "List of resolved hosts is:\n";
+  std::cout << "==========================\n";
+  auto hosts = parser.hosts();
+  for (auto &host : hosts) {
+    std::cout << host.id << "\n";
+    std::cout << "Human-readable IP: " << host.ipReadable() << "\n";
+    std::cout << "Machine-readable IP: " << host.ip << "\n";
+    std::cout << "Human-readbale Port: " << host.portReadable() << "\n";
+    std::cout << "Machine-readbale Port: " << host.port << "\n";
+    std::cout << "\n";
+  }
+  std::cout << "\n";
+
+  std::cout << "Path to output:\n";
+  std::cout << "===============\n";
+  std::cout << parser.outputPath() << "\n\n";
+
+  std::cout << "Path to config:\n";
+  std::cout << "===============\n";
+  std::cout << parser.configPath() << "\n\n";
+
+  std::cout << "Doing some initialization...\n\n";
+
+  std::cout << "Broadcasting and delivering messages...\n\n";
+
+  // PerfectLinks application(parser);
+  // FIFO application(parser);
+  LatticeAgreementApplication application(parser);
+  signal_handler = application.GetSignalHandler();
+
+  application.Start();
+  application.Run();
+  application.Stop();
+
+  // After a process finishes broadcasting,
+  // it waits forever for the delivery of messages.
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::hours(1));
+  }
+
+  return 0;
+}
