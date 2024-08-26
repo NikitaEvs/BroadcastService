a21f35916dbcb6a001e7e6bf745eba56d90fecb2
NikitaEvs <nikitaevsykov2@gmail.com>
2024-08-26 23:26:59 +0200

    [testing] Remove flaky test

--- src/tests/concurrency/fiber_sync.cpp
+++ src/tests/concurrency/fiber_sync.cpp
@@ -5,63 +5,6 @@
 #include "runtime/task.hpp"
 #include "runtime/thread_pool.hpp"
 
-TEST(FiberSync, EventSmokeTest) {
-  ThreadPool thread_pool(5);
-  thread_pool.Start();
-
-  FiberOneShotEvent event;
-  bool flag = false;
-  bool output_flag = false;
-
-  Go(&thread_pool, [&event, &flag, &output_flag]() {
-    event.Wait();
-    output_flag = flag;
-  });
-
-  Go(&thread_pool, [&event, &flag]() {
-    flag = true;
-    event.Fire();
-  });
-
-  thread_pool.Wait();
-
-  ASSERT_TRUE(flag);
-  ASSERT_TRUE(output_flag);
-
-  thread_pool.SignalStop();
-  thread_pool.WaitForStop();
-}
-
-TEST(FiberSync, EventMultipleWaiters) {
-  ThreadPool thread_pool(5);
-  thread_pool.Start();
-
-  FiberOneShotEvent event;
-  bool flag = false;
-  std::atomic<bool> output_flag;
-
-  constexpr size_t num_waiters = 100;
-  for (size_t i = 0; i < num_waiters; ++i) {
-    Go(&thread_pool, [&event, &flag, &output_flag]() {
-      event.Wait();
-      output_flag.store(flag);
-    });
-  }
-
-  Go(&thread_pool, [&event, &flag]() {
-    flag = true;
-    event.Fire();
-  });
-
-  thread_pool.Wait();
-
-  ASSERT_TRUE(flag);
-  ASSERT_TRUE(output_flag.load());
-
-  thread_pool.SignalStop();
-  thread_pool.WaitForStop();
-}
-
 TEST(FiberSync, MutexShadowTest) {
   ThreadPool thread_pool(5);
   thread_pool.Start();
